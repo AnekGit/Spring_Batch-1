4a81ba7fe80d11a4fdbe9072520a4166069dc6a2
AnekGit <anekkumarsingh01@outlook.com>
2019-07-12 15:39:48 +0530

    First Spring Demo :- Dependency Injection

--- springcore/src/main/java/com/spring/tutorial/Lab1.java
+++ springcore/src/main/java/com/spring/tutorial/Lab1.java
@@ -11,8 +11,8 @@ public class Lab1 {
         System.out.println("Hey there ");
 
         //creates spring container 
-       ApplicationContext context = new AnnotationConfigApplicationContext(Lab1Configuration.class);
-     //  ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(Lab1Configuration.class);
+       //  ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         A a = (A) context.getBean("aBean");
         System.out.println(a);
         Hello h = (Hello)context.getBean("helloBean");
